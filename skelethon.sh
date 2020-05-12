933c91e2af2869c436ec0e1204ba0583aed3363f
Eric Guimaraes de Sousa Silva <eric.silva@paygo.com.br>
2020-05-11 23:37:20 -0300

    BDD definition on first feature + Initial skelethon.sh

---
+++ skelethon.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+##################
+# Backer Package #
+##################
+#
+# Name: skelethon.sh
+# Author: Eric Guimarães
+# Contacts:
+#   - github.com/fewbits
+# Company: Lowbit Inc
+# Date: 2020-05-11
+# Version: 0.1.0
+#
+################
+# Dependencies #
+################
+#
+# source ./skelethon.sh # Myself (bad idea)
+#
+##########
+# Config #
+##########
+#
+# bpmDebug=false # Debug flag (true/false)
+#
+#############
+# Functions #
+#############
+#
+# bpmGetHelp() { # Displays a help message
+#   echo "Help Message" 
+# }
+#
+##########
+# Script #
+##########
+#
+# echo "Hello, World!"
